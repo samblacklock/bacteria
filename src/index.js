827d80efc7a0bdf058d70d640d30332683ce1b21
Sam Blacklock <samblacklock@me.com>
2019-09-02 21:55:58 +0100

    refactor: use stdin/stdout

--- src/index.js
+++ src/index.js
@@ -1,62 +1,50 @@
-import { importFile, buildFile, writeFile, sendPrompt } from './utils'
+import { createInterface } from 'readline'
+import { formatOutput, sendPrompt } from './utils'
 import Grid from './lib/Grid'
+
+/**
+ * Handle initial user input
+ */
+const getInput = () => {
+  const input = []
+
+  const rl = createInterface({
+    input: process.stdin
+  })
+
+  rl.on('line', line => {
+    input.push(line)
+    if (line === 'end') rl.close()
+  })
+
+  rl.on('close', () => {
+    processInput(input)
+  })
+}
+
+/**
+ * Perform transform on inputted values
+ * @param {string[]} input - provided input
+ */
+const processInput = async input => {
+  const grid = new Grid(input)
+  const nextGeneration = grid.performGeneration()
+  const file = formatOutput(nextGeneration)
+
+  process.stdout.write(`\n${file}\n`)
+
+  const { repeat } = await sendPrompt('Repeat?', 'confirm', 'repeat')
+
+  if (repeat) {
+    processInput(nextGeneration.map(el => el.coords))
+  } else {
+    process.exit(0)
+  }
+}
 
 /**
  * Main entry point to program - handle file import and generation
  */
 ;(async () => {
-  const { filename } = await sendPrompt(
-    'Enter filename of input data',
-    'text',
-    'filename'
-  )
-
-  try {
-    const data = await importFile(filename)
-    const grid = new Grid(data)
-
-    const { value } = await sendPrompt('CSV imported. Perform one generation?')
-
-    if (value) {
-      const nextGeneration = grid.performGeneration()
-      if (nextGeneration) complete(nextGeneration)
-    }
-  } catch ({ path }) {
-    console.error(`Could not import '${path}'`)
-  }
+  getInput()
 })()
-
-/**
- * Handle outputting new generation as .csv. Write to stdout and optionally to file
- * @param {Cell[]} nextGeneration - the next generation
- */
-const complete = async nextGeneration => {
-  const file = buildFile(nextGeneration)
-
-  process.stdout.write(file)
-
-  const { rerun } = await sendPrompt(
-    `Rerun with this dataset?`,
-    'confirm',
-    'rerun'
-  )
-
-  if (rerun) {
-  } else {
-    const { write } = await sendPrompt(
-      `Write output to a file?`,
-      'confirm',
-      'write'
-    )
-
-    if (write) {
-      const { path } = await sendPrompt('Enter filename', 'text', 'path')
-
-      try {
-        writeFile(path, file)
-      } catch {
-        console.error('There was a problem saving the file')
-      }
-    }
-  }
-}
--- src/utils.js
+++ src/utils.js
@@ -1,49 +1,10 @@
-import { createReadStream, writeFile as _writeFile } from 'fs'
-import parse from 'csv-parse'
 import prompts from 'prompts'
 
-const parser = parse({
-  relax_column_count: true
-})
-
 /**
- * Import .csv file and parse to readable format
- * @param {string} filename - filename as string
- */
-const importFile = filename =>
-  new Promise((resolve, reject) => {
-    const data = []
-    const stream = createReadStream(filename)
-    stream
-      .pipe(parser)
-      .on('data', row => {
-        // Ignore "end" statement
-        if (row[0] === 'end') return
-        data.push(row.toString())
-      })
-      .on('end', () => resolve(data))
-
-    stream.on('error', err => reject(err))
-  })
-
-/**
- * Write file to a given path
- * @param {string} path - the provided file path
- * @param {string} file - the file data to write
- */
-const writeFile = (path, file) =>
-  new Promise((resolve, reject) => {
-    _writeFile(path, file, err => {
-      if (err) reject(err)
-      resolve(console.log('The file was saved!'))
-    })
-  })
-
-/**
- * Transform new generation into format for output file
+ * Transform new generation into correct format for output
  * @param {Cell[]} arr - array of cells
  */
-const buildFile = arr => {
+const formatOutput = arr => {
   const formatted = arr.map(cell => `${cell.coords}\n`)
 
   formatted.sort((a, b) => {
@@ -80,4 +41,4 @@ const sendPrompt = (message, type = 'confirm', name = 'value') =>
  */
 const unique = data => [...new Set(data)]
 
-export { importFile, buildFile, writeFile, sendPrompt, unique }
+export { formatOutput, sendPrompt, unique }
